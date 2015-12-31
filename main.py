0714883a2427d48b4e93b55c1d8efd3cba9040fb
Kashav Madan <kshvmdn@gmail.com>
2015-12-31 00:28:17 -0500

    Finalized all game output + formatted
    TODO: Full view for single game (inc. quarterly points, location, etc)

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from datetime import date as d
 
 URL = 'http://data.nba.com/data/json/cms/noseason/scoreboard/{0}/games.json'
 
-# date = d.today().strftime('%Y%m%d') if len(sys.argv) == 1 else sys.argv[1]
-date = '20151230'
+date = d.today().strftime('%Y%m%d') if len(sys.argv) == 1 else str(sys.argv[1])
 
 
 def serve(date):
-    games = parse(fetch(date))
+
+    # check for date out of range
+    try:
+        games = parse(fetch(date))
+    except ValueError:
+        games = parse(fetch(d.today().strftime('%Y%m%d')))
+
     if len(games) == 0:  # no games
         return 'No games for provided date'
 
@@ -20,12 +25,15 @@ def serve(date):
         prompt += game_str.format(i + 1, games[i]['visitor']['abbr'], games[i]['home']['abbr'])
         prompt += '\n'
     prompt += str(len(games) + 1) + ': All\n'
-    choice = int(input(prompt))
 
-    if choice == len(games) + 1:
-        return html.unescape(prep_all(games))
+    choice = input(prompt)
+    while not choice.isnumeric():
+        choice = input(prompt)
+
+    if int(choice) == len(games) + 1:
+        return stringify_all(games)
     else:
-        return '\n' + html.unescape(prep_single(games[choice - 1]))
+        return stringify_single(games[int(choice) - 1])
 
 
 def fetch(date):
@@ -34,39 +42,36 @@ def fetch(date):
 
 def parse(response):
     games = []
-
     for game in response['sports_content']['games']['game']:
-        g = {}
-        g['loc'] = '{0} - {1}, {2}'.format(game['arena'], game['city'], game['state'])
-        g['date'], g['time'] = game['date'], game['time']
-        g['period_status'] = game['period_time']['period_status']
-        g['game_clock'] = game['period_time']['game_clock']
-        g['status'] = '{0} - {1}'.format(g['period_status'], g['game_clock'])
+        g = {'loc': '{0} - {1}, {2}'.format(game['arena'], game['city'], game['state']),
+             'date': game['date'], 'time': game['time'],
+             'period_status': game['period_time']['period_status'],
+             'game_clock': game['period_time']['game_clock'],
+             'status': '{0} - {1}'.format(g['period_status'], g['game_clock'])}
         if g['game_clock'] == '':
             g['status'] = g['period_status']
         for x in ('visitor', 'home'):
-            g[x] = {
-                'abbr': game[x]['abbreviation'],
-                'city': game[x]['city'],
-                'name': game[x]['nickname'],
-                'score': game[x]['score']
-            }
-
+            g[x] = {'abbr': game[x]['abbreviation'], 'city': game[x]['city'],
+                    'name': game[x]['nickname'], 'score': game[x]['score']}
         games.append(g)
     return games
 
 
-def prep_all(games):
+def stringify_all(games):
     output = ''
     for game in games:
-        output += '\n' + prep_single(game)
-    return output
+        output += '\n'
+        team_1, score_1 = game['visitor']['name'].ljust(14), game['visitor']['score'].rjust(3)
+        team_2, score_2 = game['home']['name'].rjust(14), game['home']['score'].ljust(3)
+        output += '{0} {1} : {2} {3} [{4}]'.format(team_1, score_1, score_2, team_2, game['status'])
+    return html.unescape(output)
 
 
-def prep_single(game):
+def stringify_single(game):
     team_1, score_1 = game['visitor']['name'].ljust(14), game['visitor']['score'].rjust(3)
     team_2, score_2 = game['home']['name'].rjust(14), game['home']['score'].ljust(3)
-    return '{0} {1} : {2} {3} [{4}]'.format(team_1, score_1, score_2, team_2, game['status'])
+    output = '{0} {1} : {2} {3} [{4}]'.format(team_1, score_1, score_2, team_2, game['status'])
+    return html.unescape(output)
 
 if __name__ == '__main__':
     print(serve(date))

ba4984c1f09fd3970751740eba11a53406cb673c
Kashav Madan <kshvmdn@gmail.com>
2016-02-22 16:49:21 -0500

    Parse submission comments for stream urls

--- streams.py
+++ streams.py
@@ -1,15 +1,18 @@
+import bs4
+import html
 import requests
-from pprint import pprint
 
-# STREAMS_URL = 'https://www.reddit.com/r/nbastreams.json?limit=20'
-STREAMS_URL = 'https://api.myjson.com/bins/40flf'  # reddit shut me down :(
+STREAMS_URL = 'http://www.reddit.com/r/nbastreams.json?limit=20'
+# STREAMS_URL = 'https://api.myjson.com/bins/40flf'
 
 
 def main(teams):
     submission = get_submission(request(STREAMS_URL), teams)
     if submission is not None:
         comments_url = 'http://reddit.com' + submission['permalink'] + '.json'
-        print(comments_url)
+        # comments_url = 'https://api.myjson.com/bins/3f7pf'
+        streams = get_streams(request(comments_url))
+        return streams if len(streams) > 0 else None
 
 
 def request(url):
@@ -21,5 +24,15 @@ def get_submission(response, teams):
         if all(team in submission['data']['title'] for team in teams):
             return submission['data']
 
+
+def get_streams(response):
+    streams = []
+    for comment in response[1]['data']['children']:
+        comment_body = html.unescape(comment['data']['body_html'])
+        soup = bs4.BeautifulSoup(comment_body, 'html.parser')
+        if soup.find('a'):
+            streams.append(soup.find('a')['href'])
+    return streams
+
 if __name__ == '__main__':
-    main(['Toronto Raptors', 'Memphis Grizzlies'])
+    print(main(['Toronto Raptors', 'Memphis Grizzlies']))

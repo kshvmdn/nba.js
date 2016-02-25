afc18ff91bde4e6e6da554c7f9e520e5cac89fa2
Kashav Madan <kshvmdn@gmail.com>
2016-02-25 17:29:33 -0500

    Add comment parser; get stream links for any (2) team(s)

--- streams.py
+++ streams.py
@@ -1,6 +1,22 @@
+from bs4 import BeautifulSoup
+import html
 import praw
+
 r = praw.Reddit(user_agent='nba_stream_parser')
 
-submissions = r.get_subreddit('nbastreams').get_hot(limit=10)
-for submission in submissions:
-    print(submission.selftext_html)
+
+def get_streams_for_team(teams):
+    teams.append('Game Thread')
+    submissions = r.get_subreddit('nbastreams').get_hot(limit=20)
+    streams = []
+    for submission in submissions:
+        if all(team in submission.title for team in teams):
+            for comment in submission.comments:
+                soup = BeautifulSoup(
+                    html.unescape(comment.body_html), 'html.parser')
+                if soup.find('a'):
+                    streams.append(soup.find('a')['href'])
+    return streams
+
+if __name__ == '__main__':
+    print(get_streams_for_team(['Spurs']))

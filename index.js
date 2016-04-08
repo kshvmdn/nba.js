0205b11cca38a396c4d329d87081e52cefdd01b5
Kashav Madan <kshvmdn@gmail.com>
2016-04-08 02:14:59 -0400

    Retrieve and parse json data

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 'use strict';
 
 const chalk = require('chalk');
+// const chalk = require('chalk');
 const got = require('got');
 const Promise = require('pinkie-promise');
+const _ = require('underscore');
+const Table = require('cli-table');
+
+const options = {
+  json: true,
+  timeout: 8000,
+  retries: 3,
+  headers: {
+    'user-agent': 'https://github.com/kshvmdn/nba-games'
+  }
+};
+
+const getHost = date => `http://data.nba.com/data/5s/json/cms/noseason/scoreboard/${date}/games.json`;
+
+const parse = data => {
+  return _.reduce(data.sports_content.games.game, (memo, game) => {
+    memo.push({
+      arena: game.arena,
+      location: `${game.city}, ${game.state}`,
+      period: {
+        status: game.period_time.period_status,
+        clock: game.period_time.game_clock
+      },
+      visitor: {
+        team: {
+          abbr: game.visitor.abbreviation,
+          name: game.visitor.nickname,
+          city: game.visitor.city
+        },
+        score: game.visitor.score
+      },
+      home: {
+        team: {
+          abbr: game.home.abbreviation,
+          name: game.home.nickname,
+          city: game.home.city
+        },
+        score: game.home.score
+      }
+    });
+    return memo;
+  }, []);
+};

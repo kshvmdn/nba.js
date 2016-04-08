3b1a51ec14d4229141eb10485a81dd890c13f63c
Kashav Madan <kshvmdn@gmail.com>
2016-04-08 14:51:02 -0400

    Use shorthand exports

--- src/options.js
+++ src/options.js
@@ -1,26 +1,22 @@
-const request = {
-  json: true,
-  timeout: 8000,
-  retries: 3,
-  headers: {
-    'user-agent': 'https://github.com/kshvmdn/nba-games'
-  }
-}
-
-const table = {
-  head: ['AWAY', '', '', 'HOME', 'STATUS'],
-  chars: {'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗',
-          'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝',
-          'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼',
-          'right': '║' , 'right-mid': '╢' , 'middle': '│' },
-  colAligns: ['right', 'middle', 'middle', 'left', 'left'],
-  style: {
-    head: ['blue'],
-    border: ['white']
-  }
-}
-
 module.exports = {
-  request: request,
-  table: table
-}
+  request: {
+    json: true,
+    timeout: 8000,
+    retries: 3,
+    headers: {
+      'user-agent': 'https://github.com/kshvmdn/nba-games'
+    }
+  },
+  table: {
+    head: ['AWAY', '', '', 'HOME', 'STATUS'],
+    chars: {'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗',
+            'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝',
+            'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼',
+            'right': '║', 'right-mid': '╢', 'middle': '│'},
+    colAligns: ['right', 'middle', 'middle', 'left', 'left'],
+    style: {
+      head: ['blue'],
+      border: ['white']
+    }
+  }
+};

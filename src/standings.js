c8b0b32785fb14ba18c035759f83532a90da5a30
Kashav Madan <kshvmdn@gmail.com>
2016-04-11 21:12:22 -0400

    Get team name, win-loss

--- src/standings.js
+++ src/standings.js
@@ -8,9 +8,32 @@ const utils = require('./utils');
 
 const getHost = year => `http://data.nba.com/data/json/cms/${year}/league/standings.json`;
 
+const parse = res => {
+  if (res.statusCode !== 200) {
+    throw new Error(`Couldn't retrieve standings data.`);
+  }
+
+  let teams = res.body.sports_content.standings.team;
+  return _.reduce(teams, (memo, team) => {
+    memo.push({
+      'team': {
+        'abbr': team.abbreviation,
+        'name': team.nickname,
+        'city': team.name
+      },
+      'wins': team.team_stats.wins,
+      'losses': team.team_stats.losses,
+    });
+    return memo
+  }, []);
+}
+
 module.exports = args => {
   let year = args.year;
   return got(getHost(year), options.request)
+    .then(response => {
+      return parse(response);
+    })
     .then(response => {
       console.log(response);
     })

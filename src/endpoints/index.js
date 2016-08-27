f8f38cdbef2934b399169b6db719e6cd25448978
Kashav Madan <kshvmdn@gmail.com>
2016-08-27 13:59:22 -0400

    Inline export

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,15 +1,10 @@
 import * as worker from './worker'
 import { ENDPOINTS as c } from './../constants'
 
-function allPlayers (query, cb) {
+export function allPlayers (query, cb) {
   return worker.run(c.ALL_PLAYERS, query, cb)
 }
 
-function playerProfile (query, cb) {
+export function playerProfile (query, cb) {
   return worker.run(c.PLAYER_PROFILE, query, cb)
-}
-
-export {
-  allPlayers,
-  playerProfile
 }

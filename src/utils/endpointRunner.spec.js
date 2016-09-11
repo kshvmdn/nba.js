3b0b361588fa6bcb6034250d3e590534e6fb3736
Kashav Madan <kshvmdn@gmail.com>
2016-09-11 14:55:25 -0400

    Add endpoint runner/worker tests

--- src/utils/endpointRunner.spec.js
+++ src/utils/endpointRunner.spec.js
@@ -1,2 +1,38 @@
 import should from 'should'
 import * as runner from './endpointRunner'
+import * as worker from './endpointWorker'
+
+let r = runner.run(worker.work)
+
+describe('utils/endpointRunner', () => {
+  it('should error out if no worker is provided', (done) => {
+    runner.run()(null, null, (err, res) => {
+      should.exist(err)
+      err.should.have.property('message')
+      err.message.should.eql('Expected worker function.')
+      done()
+    })
+  })
+
+  it('should error out when an invalid endpoint is provided', (done) => {
+    r('INVALID_ENDPOINT', null, (err, res) => {
+      should.exist(err)
+      err.should.have.property('message')
+      err.message.should.eql('Invalid endpoint type.')
+      done()
+    })
+  })
+
+  it('should respond with Promise', (done) => {
+    r('ALL_PLAYERS', null).should.be.Promise()
+    done()
+  })
+
+  it('should respond with error-first callback', (done) => {
+    r('ALL_PLAYERS', null, (err, res) => {
+      should.not.exist(err)
+      should.exist(res)
+      done()
+    })
+  }).timeout(10000)
+})
--- src/utils/endpointWorker.spec.js
+++ src/utils/endpointWorker.spec.js
@@ -1,2 +1,29 @@
 import should from 'should'
 import * as worker from './endpointWorker'
+import { ENDPOINTS as e } from './../constants'
+
+let w = worker.work
+
+describe('utils/endpointWorker', () => {
+  it('should respond with a Promise', (done) => {
+    w(e.ALL_PLAYERS, null).should.be.Promise()
+    done()
+  })
+
+  it('should respond with an error-first cb', (done) => {
+    w(e.ALL_PLAYERS, null, (err, res) => {
+      should.not.exist(err)
+      should.exist(res)
+      done()
+    })
+  }).timeout(10000)
+
+  it('should throw HTTPError when invalid query params are provided', (done) => {
+    w(e.PLAYER_AWARDS, null, (err, res) => {
+      should.exist(err)
+      err.should.have.property('body')
+      err.body.should.eql('The field PlayerID must be between 1 and 2147483647.')
+      done()
+    })
+  }).timeout(10000)
+})

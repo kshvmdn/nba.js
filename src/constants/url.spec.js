c98909372581f30cdb0b82c8fc0b2e7eaed4b788
Kashav Madan <kshvmdn@gmail.com>
2016-10-08 23:46:29 -0400

    Fix failing constants/url test

--- src/constants/url.spec.js
+++ src/constants/url.spec.js
@@ -2,10 +2,10 @@ import should from 'should'
 import * as constants from './url'
 
 describe('constants/url', () => {
-  it('should export BASE_URL', (done) => {
-    constants.BASE_URL.should.not.be.empty()
-    constants.BASE_URL.should.startWith('http')
-    constants.BASE_URL.should.endWith('com')
+  it('should export URL object', (done) => {
+    constants.URL.should.not.be.empty()
+    constants.URL.should.have.property('data')
+    constants.URL.should.have.property('stats')
     done()
   })
 })
